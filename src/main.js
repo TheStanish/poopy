61e431c87c5414f408851b2c493547d200b66469
TheStanish <tstan2310@gmail.com>
2023-05-24 16:55:58 -0400

    logo

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: __dirname + '/icon.png',
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
