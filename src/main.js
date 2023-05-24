f3019052de9f4113eed23ef178cfdc3951613fd1
TheStanish <tstan2310@gmail.com>
2023-05-24 16:46:17 -0400

    merp

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + '/icon.png',
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
